de9a10445ba981b8f939377af4727b99a46d6339
Nina Boosheri <nboosheri@gmail.com>
2019-05-04 12:27:26 -0700

    adjust sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,20 @@
 class SessionsController < ApplicationController
-    def new
-    end
 
     def create
-        #   user = User.find_by_email(params[:email])
-        #   if user&.authenticate(params[:password])
-        #   # Equivalent to:
-        #   # if user && user.authenticate(params[:password])
-        #     session[:user_id] = user.id
-        #     redirect_to root_path, notice: 'Logged In'
-        #   else
-        #     flash[:alert] = "Wrong email or password"
-        #     render :new
-        #   end
+          user = User.find_by_email(params[:email])
+          if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: 'Logged In'
+          else
+            flash[:alert] = "Wrong email or password"
+            render :new
+          end
     end
 
     def destroy
-        # session[:user_id] = nil
-        # flash[:notice] = "logged out"
-        # redirect_to root_path
+        session[:user_id] = nil
+        flash[:notice] = "logged out"
+        redirect_to root_path
     end
 end
 
