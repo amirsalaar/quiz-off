4cfa5c004e6f690311cc9ea6f50d7bef18667f57
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-04 17:28:40 -0700

    Implement sign out and edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
   before_action :find_user, only: [:edit, :update]
   def new
     @user = User.new
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Registered successfully!"
       redirect_to root_path
     else
@@ -32,7 +34,7 @@ class UsersController < ApplicationController
   end
 
   def find_user
-    @user = User.find(current_user)
+    @user = User.find(current_user.id)
   end
   
 end
