53626061cfb4e1072c866602bc214a56e7f3b8c4
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-05 11:59:07 -0700

    Add true value to answers made when creating a question

--- db/migrate/20190505174222_add_is_correct_to_answers.rb
+++ db/migrate/20190505174222_add_is_correct_to_answers.rb
@@ -1,5 +1,5 @@
 class AddIsCorrectToAnswers < ActiveRecord::Migration[5.2]
   def change
-    add_column :answers, :is_correct, :boolean
+    add_column :answers, :is_correct, :boolean, :default => false
   end
 end
