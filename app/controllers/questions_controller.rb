b5004334ada685399c0a7698c2b9f47b7deebbbe
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-05 15:53:27 -0700

    Fix create question bug

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -45,7 +45,7 @@ class QuestionsController < ApplicationController
     private
 
     def question_params
-        params.require(:question).permit(:body, answers_attributes: [:body, is_correct: true])
+        params.require(:question).permit(:body, answers_attributes: [:body, :is_correct])
     end
 
     def find_quiz
