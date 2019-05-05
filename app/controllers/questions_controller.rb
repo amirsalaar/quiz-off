861b7e5c4e991f70d9d61b7a0cbd209e7d2865e7
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-05 00:47:29 -0700

    Implement create question with nested answer form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,7 @@
 class QuestionsController < ApplicationController
+    before_action :authenticate_user!, except: [:show]
     before_action :find_question, only: [ :edit, :update, :destroy]
+    before_action :find_quiz, only: [:create, :edit, :update]
 
     def new
         @question = Question.new
@@ -7,11 +9,12 @@ class QuestionsController < ApplicationController
 
     def create
         @question = Question.new question_params
-        @question.user = current_user
+        @question.quiz = @quiz
+        @question.quiz.user = current_user
         if @question.save
-            # redirect somewhere
-            redirect_to quiz_questions_path	
+            redirect_to quiz_path(@quiz)	
         else
+            flash[:danger] = @question.errors.full_messages.join(', ')
             render :new 
         end
     end
@@ -42,7 +45,11 @@ class QuestionsController < ApplicationController
     private
 
     def question_params
-        params.require(:question).permit(:body)
+        params.require(:question).permit(:body, answers_attributes: [:body])
+    end
+
+    def find_quiz
+        @quiz = Quiz.find(params[:quiz_id])
     end
 
     def find_question
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,8 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :answers, dependent: :destroy
 
-  # We need this line for nested fields to include answers edit fields in question#edit
-  accepts_nested_attributes_for :answers
+  accepts_nested_attributes_for :answers, allow_destroy: true, reject_if: proc { |answer| answer['body'].blank? }
+
+  validates_presence_of :answers
+  validates :body, presence: true
 end
