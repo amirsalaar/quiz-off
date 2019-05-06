c373e7830440d921ccf3a912f9a9f24b3dc3fd3d
Anson Lam <anson.lam94@gmail.com>
2019-05-05 19:53:23 -0700

    Add attempts#create action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -64,7 +64,7 @@ class QuestionsController < ApplicationController
     private
 
     def question_params
-        params.require(:question).permit(:body, answers_attributes: [:body, :is_correct])
+        params.require(:question).permit(:body, answers_attributes: [{body: :body, is_correct: true}])
     end
 
     def find_quiz
