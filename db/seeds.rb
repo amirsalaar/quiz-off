7b7746ef86a16122bc2904dbc97ffad8577fe82a
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-07 16:08:38 -0700

    Include seeding file and fix conflicts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,58 +1,58 @@
-PASSWORD = "supersecret"
+# frozen_string_literal: true
+
+PASSWORD = 'supersecret'
 Question.destroy_all
 Answer.destroy_all
 Attempt.destroy_all
 User.destroy_all
 Quiz.destroy_all
 
-super_user = User.create({
-  first_name: "Jon",
-  last_name: "Snow",
-  email: "js@winterfell.gov",
+super_user = User.create(
+  first_name: 'Jon',
+  last_name: 'Snow',
+  email: 'js@winterfell.gov',
   password: PASSWORD,
-  role: 2,
-  total_points: rand(0..20_000)
-})
+  role: 1
+)
 
 10.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
-  User.create({
+  User.create(
     first_name: first_name,
     last_name: last_name,
     email: "#{first_name.downcase}.#{last_name.downcase}@example.com",
     password: PASSWORD,
     role: rand(1..2),
-    total_points: rand(0..20_000)
-  })
+    total_points: rand(0..1000)
+  )
 end
 
 users = User.all
-
-
+teachers = User.where(role: 1)
+students = User.where(role: 2)
 
 50.times do
   created_at = Faker::Date.backward(365 * 5)
-  quiz = Quiz.create({
+  quiz = Quiz.create(
     title: Faker::TvShows::Simpsons.location,
-    description: (Faker::Hacker.say_something_smart)[0..20],
-    level: (1 +rand(3)),
-    points: [10,15,20,30][rand(4)],
+    description: Faker::Hacker.say_something_smart[0..20],
+    level: rand(1..3),
+    points: [10, 15, 20, 30][rand(4)],
     created_at: created_at,
     updated_at: created_at,
-    user: users.sample
-  })
+    user: teachers.sample
+  )
   5.times do
-    list_answers = []
-    4.times do
-        list_answers << { body: (Faker::Hacker.say_something_smart)[0..15] }
+    question = Question.new(
+      quiz: quiz,
+      body: Faker::GreekPhilosophers.quote
+    )
+    question.answers << Answer.new(body: Faker::Hacker.say_something_smart[0..15], is_correct: true)
+    question.save
+    3.times do
+      Answer.create(body: Faker::Hacker.say_something_smart[0..15], is_correct: false, question: question)
     end
-    list_answers[0].merge!(is_correct: true)
-     question = Question.create({
-       quiz_id: quiz.id,
-      body: Faker::GreekPhilosophers.quote,
-      answers_attributes: list_answers
-    })
   end
 end
 
@@ -62,8 +62,8 @@ questions = Question.all
 
 answers = Answer.all
 
-puts Cowsay.say("Generated #{ quizzes.count } quizzes", :ghostbusters)
-puts Cowsay.say("Generated #{ questions.count } questions", :dragon)
-puts Cowsay.say("Generated #{ answers.count } answers", :stegosaurus)
-puts Cowsay.say("Generated #{ users.count } users", :beavis)
-puts Cowsay.say("Login with #{ super_user.email } and password: #{PASSWORD}", :koala)
+puts "Generated #{quizzes.count} quizzes"
+puts "Generated #{questions.count} questions"
+puts "Generated #{answers.count} answers"
+puts "Generated #{users.count} users"
+puts "Login with #{super_user.email} and password: #{PASSWORD}"
